5114ec0014e2000b1241a0677db4a76158a38b1a
Dawid Tracz <dawid.tracz@o2.pl>
2017-11-25 20:22:33 +0100

    commit of first files

---
+++ KnowNow/src/main/java/com/dtdb/KnowNow/KnowNowApplication.java
@@ -0,0 +1,12 @@
+package com.dtdb.KnowNow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowNowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowNowApplication.class, args);
+	}
+}
---
+++ KnowNow/src/test/java/com/dtdb/KnowNow/KnowNowApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dtdb.KnowNow;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KnowNowApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
